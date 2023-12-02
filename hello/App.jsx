d8e5c002834111336b2aaf6c3b0c8744d1a29ba8
butto safwan <butto.safwan@live.biu.ac.il>
2023-12-02 23:24:36 +0200

    t

--- hello/App.jsx
+++
@@ -1,7 +0,0 @@
-import AppNavigator from './src/navigation/AppNavigator'; // Ensure this path is correct based on your project structure
-
-const App = () => {
-  return <AppNavigator />;
-};
-
-export default App;
